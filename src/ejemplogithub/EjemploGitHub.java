04304277dbebde7a766a696fe51af8f999e1a3e2
Zamora <Zamora@DESKTOP-998NS06>
2022-11-15 00:51:02 -0600

    Prueba num 1

---
+++ src/ejemplogithub/EjemploGitHub.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplogithub;
+
+/**
+ *
+ * @author Zamora
+ */
+public class EjemploGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("JZ");
+    }
+    
+}
